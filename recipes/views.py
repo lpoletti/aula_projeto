bcba97cc4e09a2812f2cde3e9620809aa6ef4b7d
Luan Poletti <luanpoletti@gmail.com>
2023-09-09 16:44:33 -0300

    settings/views

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     # return HTTP RESPONSE
-    return render(request,'home.html')
+    return render(request,'recipes/home.html')
 
 def contato(request):
     # return HTTP RESPONSE
